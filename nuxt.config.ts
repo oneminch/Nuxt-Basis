8fb26381240a6d2b763682d0d4e4ff4ea3437fca
oneminch <27751688+oneminch@users.noreply.github.com>
2025-10-04 16:10:31 -0400

    refactor: add layer name

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,10 @@ const currentDir = dirname(fileURLToPath(import.meta.url));
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  $meta: {
+    name: "nuxt-basis"
+  },
+
   css: [join(currentDir, "./app/assets/css/tailwind.css")],
 
   devtools: { enabled: true },
